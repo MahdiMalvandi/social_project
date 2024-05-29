de42c8cd89f4e0cc62d5d73847c0b2d6f8e4daed
mahdi <mahdimalvandi6@gmail.com>
2024-05-29 22:37:24 +0330

    edit codes

--- posts/models.py
+++ posts/models.py
@@ -11,7 +11,7 @@ from taggit.managers import TaggableManager
 
 class ActiveStoryManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(created__gte=timezone.now() - timezone.timedelta(seconds=10))
+        return super().get_queryset().filter(created__gte=timezone.now() - timezone.timedelta(days=1))
 
 
 class ActiveManager(models.Manager):
